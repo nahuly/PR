6d2e5a817495051359b227ada3210f5ff33024c1
nahuly <58017922+nahuly@users.noreply.github.com>
2021-11-01 22:01:40 +0900

    Add files via upload

---
+++ 백준 나이순 정렬.py
@@ -0,0 +1,12 @@
+import sys
+
+inp=int(sys.stdin.readline())
+
+for _ in range(inp):
+    a,b=map(int,sys.stdin.readline().rsplit())
+    print(a+b)
+
+
+a=int(input())
+for i in range(1,a+1):
+    print(i)
